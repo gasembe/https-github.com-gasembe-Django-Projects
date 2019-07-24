137b62bb61a44fa01190cffc9328cc18c0d11987
Abdi Adam <abdiadam336@hotmail.com>
2019-07-24 11:43:53 +0300

    Initial blog Migration

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # Add your apps here to enable them
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
